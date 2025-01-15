4f642b07c274e41227d49a4607f5cd16d015859a
Jeong-MinE <susua6536@naver.com>
2025-01-15 23:52:52 +0900

    App.test

--- react-study/src/index.js
+++ react-study/src/index.js
@@ -5,6 +5,7 @@ import App3 from './study/App3';
 import App4 from './study/App4';
 import App5 from './study/App5';
 import App6 from './study/App6';
+import App7 from './study/App7';
 // default가 없다면 export는 {} 넣어서 가져와야함.
 // default는 하나만 존재하기 때문에 변수명 수정이 가능하다.
 
@@ -18,7 +19,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(<App3 />);
 // root.render(<App4 />);
 // root.render(<App5/>)
-root.render(<App6/>)
+// root.render(<App6/>)
+root.render(<App7/>)
 
 
 
